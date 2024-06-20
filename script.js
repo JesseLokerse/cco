cf720b2e5cf64774df1518ecf6b212274150fff1
Jesse Lokerse <loke0017@hz.nl>
2024-06-20 13:16:39 +0200

    initial1

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-let turn = true
-document.addEventListener("DOMContentLoaded", function() {
-    const gridItems = document.querySelectorAll(".button");
+const fetch = require('node-fetch');
 
-    gridItems.forEach(item => {
-        item.addEventListener("click", function() {
-            // Toggle de klasse .red op het geklikte item
-            if(turn){
-                this.classList.toggle("red");
-                turn = false
-            } else {
-                this.classList.toggle("green");
-                turn = true
-            }
-        });
-    });
+async function startNieuwSpel() {
+    const functionEndpoint = '<URL_van_je_Azure_Function>';
 
-});
-console.log(turn)
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        // Optioneel: data die naar de functie wordt gestuurd
+        body: JSON.stringify({ /* eventuele data */ }),
+    };
+
+    try {
+        const response = await fetch(functionEndpoint, requestOptions);
+        const data = await response.json();
+        console.log('Response van de Azure Function:', data);
+        // Verwerk de ontvangen data indien nodig
+    } catch (error) {
+        console.error('Fout bij het aanroepen van de Azure Function:', error);
+    }
+}
+
+// Roep de functie aan om het spel te starten
+startNieuwSpel();
